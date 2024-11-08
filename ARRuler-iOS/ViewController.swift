d144a816e5c323c1c187397bb115229ca10bc4b8
Rob Harting <robharting@hotmail.com>
2024-11-08 16:32:14 +0100

    remove previous text result measurement and complete app

--- ARRuler-iOS/ViewController.swift
+++ ARRuler-iOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,18 +49,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
             
             if let hitResult = hitTestResults.first {
                 addDot(at: hitResult)
             }
+            
         }
     }
     
     func addDot(at hitResult : ARHitTestResult) {
         let dotGeometry = SCNSphere(radius: 0.005)
-        
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.red
         
@@ -105,21 +113,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func updateText(text: String, atPosition position: SCNVector3) {
         
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
-        
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
         
         sceneView.scene.rootNode.addChildNode(textNode)
         
-        
-        
-        
-        
     }
-    
-    
 }
